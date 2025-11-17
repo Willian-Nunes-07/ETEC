322b6dd9e6c20b665a0d1b39a4f79308d7d6cf9e
Willian-Nunes-07 <williannunes@protonmail.com>
2025-11-17 11:31:17 -0300

    Add files via upload

---
+++ DS2/Agendas_13_e_14/MauiAppHotel/Views/PaginaInicio.xaml.cs
@@ -0,0 +1,39 @@
+using MauiAppHotel.Models;
+
+namespace MauiAppHotel.Views;
+
+public partial class PaginaInicio : ContentPage
+{
+    App PropriedadesApp;
+    public PaginaInicio()
+	{
+		InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            await Navigation.PushAsync(new ContratacaoHospedagem());
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+
+    private async void Button_Clicked_2(object sender, EventArgs e)
+    {
+        try
+        {
+            await Navigation.PushAsync(new Sobre());
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+}
