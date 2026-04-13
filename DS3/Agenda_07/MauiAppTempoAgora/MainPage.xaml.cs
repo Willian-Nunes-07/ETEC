3abd42ed7c46a2d709789b24e91f6776e93fa8e3
Willian-Nunes-07 <williannunes@protonmail.com>
2026-04-13 10:23:15 -0300

    Add files via upload

---
+++ DS3/Agenda_07/MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Velocidade do Vento: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility} \n" +
+                                         $"Descrição: {t.description} \n";
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else 
+                    {
+
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+
+                }else
+                {
+                    lbl_res.Text = "Por favor, insira o nome da cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+    }
+
+
+}
---
+++ DS3/Agenda_07/MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,76 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage resp = await client.GetAsync(url);
+
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        string json = await resp.Content.ReadAsStringAsync();
+
+                        var rascunho = JObject.Parse(json);
+
+                        DateTime time = new();
+                        DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                        DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+
+                        t = new()
+                        {
+                            lat = (double)rascunho["coord"]["lat"],
+                            lon = (double)rascunho["coord"]["lon"],
+                            description = (string)rascunho["weather"][0]["description"],
+                            main = (string)rascunho["weather"][0]["main"],
+                            temp_min = (double)rascunho["main"]["temp_min"],
+                            temp_max = (double)rascunho["main"]["temp_max"],
+                            speed = (double)rascunho["wind"]["speed"],
+                            visibility = (int)rascunho["visibility"],
+                            sunrise = sunrise.ToString(),
+                            sunset = sunset.ToString(),
+                        };// Fecha objeto do tempo
+                    }
+                    else
+                    {
+                        switch (resp.StatusCode)
+                        {
+                            case System.Net.HttpStatusCode.NotFound:
+                                throw new Exception("Cidade não encontrada. Por favor, verifique o nome da cidade.");
+                            case System.Net.HttpStatusCode.BadRequest:
+                                throw new Exception("Houve algo errado com a requisição. Verifique os parâmetros.");
+                            default:
+                                throw new Exception($"Erro desconhecido: {resp.StatusCode}");
+                        }
+                    }
+                }
+                catch (HttpRequestException)
+                {
+                    throw new Exception("Sem conexão com a internet. Verifique sua conexão.");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Erro ao processar a requisição: {ex.Message}");
+                }// Fecha if se o status do servidor foi de sucesso
+            }// Fecha loop using
+
+            return t;
+        }
+    }
+}
+
+
